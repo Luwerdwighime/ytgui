f237939aca5d83c1e8a8135d4fc36e3de9f99256
Sebos <sebos@nazarik>
2025-07-01 01:01:08 +0200

    git fix

--- app/src/main/java/org/nazarik/ytgui/ConsoleActivity.java
+++ app/src/main/java/org/nazarik/ytgui/ConsoleActivity.java
@@ -3,8 +3,6 @@ import android.os.Bundle;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ConsoleActivity extends AppCompatActivity {
   @Override
@@ -13,7 +11,7 @@ public class ConsoleActivity extends AppCompatActivity {
     setContentView(R.layout.activity_console);
     String filesDir = getFilesDir().getAbsolutePath();
     String gitBinDir = filesDir + "/git-bin";
-    String sshKeyPath = gitBinDir + "/.ssh/id_rsa";
+    String sshKeyPath = gitBinDir + "/ssh/id_rsa";
     String envDir = filesDir + "/ytgui-env";
     File sshKey = new File(sshKeyPath);
     if (!sshKey.exists()) {
@@ -22,28 +20,27 @@ public class ConsoleActivity extends AppCompatActivity {
       finish();
       return;
     }
-    String command = gitBinDir + "/git clone --depth=1 git@github.com:Luwerdwighime/ytgui-env.git " + envDir;
+    String command = getIntent().getStringExtra("command");
+    if (command == null) {
+      Log.e("ytgui", "No command provided");
+      setResult(RESULT_CANCELED);
+      finish();
+      return;
+    }
     try {
-      // Настройка переменной окружения для SSH
-      Map<String, String> env = new HashMap<>();
-      env.put("GIT_SSH_COMMAND", "ssh -i " + sshKeyPath);
-      ProcessBuilder pb = new ProcessBuilder(command.split("\\s+"));
-      pb.environment().putAll(env);
-      pb.directory(new File(filesDir));
-      Process process = pb.start();
-      int exitCode = process.waitFor();
-      if (exitCode == 0) {
-        Log.d("ytgui", "Git clone succeeded");
-        setResult(RESULT_OK);
-      } else {
-        Log.e("ytgui", "Git clone failed with exit code: " + exitCode);
-        setResult(RESULT_CANCELED);
-      }
+      GitUtils.runCommand(this, command, findViewById(R.id.consoleOutput), new String[]{"GIT_SSH_COMMAND=ssh -i " + sshKeyPath}, exitCode -> {
+        if (exitCode == 0) {
+          setResult(RESULT_OK);
+        } else {
+          setResult(RESULT_CANCELED);
+        }
+        finish();
+      });
     } catch (Exception e) {
       Log.e("ytgui", "Exception during git clone", e);
       setResult(RESULT_CANCELED);
+      finish();
     }
-    finish();
   }
 }
 
